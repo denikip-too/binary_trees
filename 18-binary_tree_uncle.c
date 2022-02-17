b3a99b738b977977b1b4d039205ca30f859fa69b
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-02-17 08:05:14 -0800

    finds the uncle of a node

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -7,9 +7,9 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node->parent->parent == NULL || node->parent == NULL)
-		return (NULL);
 	if (node == NULL)
+                return (NULL);
+	if (node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 	if (node->parent == node->parent->parent->right)
 		return (node->parent->parent->left);
