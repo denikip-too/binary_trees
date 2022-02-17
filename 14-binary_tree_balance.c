514ea00d5034deaf8f3c3ed01e53180015d810dd
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-02-17 11:19:09 -0800

    measures the balance factor of a binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,15 +7,15 @@
  */
 size_t _binary_tree_height(const binary_tree_t *tree)
 {
-	size_t lheight = 0, rheight = 0;
+	size_t lheight, rheight;
 
 	if (tree == NULL)
 		return (-1);
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
-	lheight = _binary_tree_height(tree->left);
-	rheight = _binary_tree_height(tree->right);
-	return (MAX(lheight, rheight) + 1);
+	lheight = _binary_tree_height(tree->left) + 1;
+	rheight = _binary_tree_height(tree->right) + 1;
+	return (MAX(lheight, rheight));
 }
 /**
  * binary_tree_balance - measures the balance factor of a binary tree
